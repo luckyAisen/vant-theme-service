4e7f869a9df381b8c21cf92b40710f6f9f4b81d4
Aisen60 <caixusheng60@gmail.com>
2021-05-16 18:03:03 +0800

    feat(server): 服务文件的修改

--- src/server.ts
+++ src/server.ts
@@ -1,72 +1,55 @@
 import express from 'express'
-import bodyParser from 'body-parser'
-import morgan from 'morgan'
-import { createStream, Generator } from 'rotating-file-stream'
-import { parseTime } from './utils'
+// import bodyParser from 'body-parser'
+import fs from 'fs-extra'
 import less from 'less'
-import path from 'path'
-import fs from 'fs'
-
+import { concatPath } from './utils'
+import config from './config'
+import logMiddlewares from './logMiddlewares'
+import { Version } from 'interface'
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const LessPluginCleanCSS = require('less-plugin-clean-css')
 
 const app = express()
 
-const port = 5000
+app.use(express.json())
 
-app.use(bodyParser.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.use(bodyParser.urlencoded({ extended: true }))
-
-function log() {
-  // 自定义token
-  morgan.token('localDate', function getDate(req) {
-    const date = new Date()
-    return date.toLocaleString()
-  })
-
-  const logName = (time: number | Date) => {
-    if (!time) return 'log.log'
-    const timeStr = parseTime(time, '{y}-{m}-{d}-{h}-{i}-{s}')
-    return `${timeStr}-file.log`
-  }
-  const logPath = process.cwd()
-  const accessLogStream = createStream(logName as Generator, {
-    size: '10M',
-    interval: '1d',
-    path: path.join(logPath, 'log'),
-  })
-  morgan.format(
-    'combined',
-    ':remote-addr - :remote-user [:localDate]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"'
-  )
-  app.use(
-    morgan('combined', {
-      stream: accessLogStream,
-    })
-  )
-}
+logMiddlewares(app)
 
 function service() {
-  log()
   app.post('/compiler', async function (req, res) {
     try {
-      const input = fs.readFileSync(`./src/styles/3.x/index.less`, 'utf8')
+      const version = req.query.version as keyof Version
+      const dir = concatPath(config.dirPath.styleDir, version, 'index.less')
+      if (!config.version[version]) throw new Error('version has no exist')
+      const input = fs.readFileSync(dir, 'utf8')
+      const global = req.body.global || {}
+      const local = req.body.local || {}
       let str = ''
-      for (const [key] of Object.entries(req.body)) {
-        str += `@${key}:${req.body[key]};\n`
+      for (const [key] of Object.entries(global)) {
+        str += `${key}:${global[key]};\n`
       }
+      for (const [key] of Object.entries(local)) {
+        str += `${key}:${local[key]};\n`
+      }
+      // console.log(global)
+      // console.log(local)
+      // console.log(input)
+      // console.log(str)
       const options = {
         plugins: [new LessPluginCleanCSS({ advanced: true })],
       }
       const { css }: { css: string } = await less.render(input + str, options)
       res.send(css)
     } catch (err) {
-      res.status(500).send(err)
+      res.status(500).send(err.message)
       console.log(err)
     }
   })
-  app.listen(port, () => console.log(`Server is listening on port ${port}!`))
+  app.listen(config.port, () =>
+    console.log(`Server is listening on port ${config.port}!`)
+  )
 }
 
 service()
