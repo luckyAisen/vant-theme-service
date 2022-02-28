6108043be65a056f64345170561cc48c14a27ad4
Aisen60 <caixusheng60@gmail.com>
2022-03-01 00:42:17 +0800

    chore: update

---
+++ scripts/action.js
@@ -0,0 +1,13 @@
+import { updateVantSouce, updateVantIconPath } from './utils.js'
+
+async function action() {
+  try {
+    await updateVantSouce()
+    await updateVantIconPath()
+  } catch (err) {
+    console.log(err)
+    process.exit(1)
+  }
+}
+
+action()
